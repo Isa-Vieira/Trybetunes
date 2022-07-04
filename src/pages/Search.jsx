11e2f5e5c09acb060a8f67a1a00b257c5240801f
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-07-04 17:09:41 -0300

    requisito-5-concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,14 +2,53 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
-  render() {
-    return (
-      <div>
-        <div data-testid="page-search" />
-        <Header />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      saveButton: true,
+    };
+  }
+
+habilitImput = (event) => {
+  const { value } = event.target;
+  const caracters2 = 2;
+  if (value.length >= caracters2) {
+    this.setState({
+      saveButton: false,
+    });
+  } else {
+    this.setState({
+      saveButton: true,
+    });
   }
 }
 
+render() {
+  const { saveButton } = this.state;
+  return (
+    <div data-testid="page-search">
+      <Header />
+      <form>
+        <label htmlFor="Pesquisar">
+          <input
+            onChange={ this.habilitImput }
+            type="text"
+            name="Pesquisar"
+            data-testid="search-artist-input"
+          />
+        </label>
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          onClick={ this.clickButton }
+          disabled={ saveButton }
+        >
+          Pesquisar
+
+        </button>
+      </form>
+    </div>
+  );
+}
+}
 export default Search;
