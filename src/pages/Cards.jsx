f42995c4d67964c3c2a0a42bd6eccf9d635d2b98
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-07-05 16:27:07 -0300

    requisito-6-concluido

---
+++ src/pages/Cards.jsx
@@ -0,0 +1,17 @@
+/* import react from 'react';
+
+class Card extends Components {
+state = {
+
+}
+
+render() {
+  return (
+    <div>
+      <p>Name</p>
+      <p>Name music</p>
+      <img Foto music </img>
+    </div>
+  );
+}
+} */
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      nameartistOrBand: '',
       saveButton: true,
+      artistAlbum: [],
+      saveButtonClaean: '',
+      loading: false,
     };
   }
+
+listAlbum = async () => {
+  this.setState({
+    loading: true,
+  });
+  const { nameartistOrBand } = this.state;
+  const value = await searchAlbumsAPI(nameartistOrBand);
+  console.log(value);
+  this.setState({
+    nameartistOrBand: '',
+    artistAlbum: value,
+    loading: false,
+  });
+}
 
 habilitImput = (event) => {
   const { value } = event.target;
@@ -15,38 +36,76 @@ habilitImput = (event) => {
   if (value.length >= caracters2) {
     this.setState({
       saveButton: false,
+      nameartistOrBand: value,
+      saveButtonClaean: value,
     });
   } else {
     this.setState({
       saveButton: true,
+      nameartistOrBand: value,
     });
   }
 }
 
 render() {
-  const { saveButton } = this.state;
+  const { saveButton, nameartistOrBand, artistAlbum,
+    saveButtonClaean, loading } = this.state;
   return (
-    <div data-testid="page-search">
+    <div
+      data-testid="page-search"
+    >
       <Header />
-      <form>
-        <label htmlFor="Pesquisar">
-          <input
-            onChange={ this.habilitImput }
-            type="text"
-            name="Pesquisar"
-            data-testid="search-artist-input"
-          />
-        </label>
-        <button
-          data-testid="search-artist-button"
-          type="button"
-          onClick={ this.clickButton }
-          disabled={ saveButton }
-        >
-          Pesquisar
+      { loading ? <Loading />
+        : (
+          <form>
+            <label htmlFor="Pesquisar">
 
-        </button>
-      </form>
+              <input
+                onChange={ this.habilitImput }
+                value={ nameartistOrBand }
+                type="text"
+                name="Pesquisar"
+                data-testid="search-artist-input"
+              />
+            </label>
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              onClick={ this.listAlbum }
+              disabled={ saveButton }
+            >
+              Pesquisar
+
+            </button>
+            <p>
+              { `Resultado de álbuns de: ${saveButtonClaean}` }
+            </p>
+          </form>)}
+      { artistAlbum[0]
+        ? (
+          artistAlbum.map((music, index) => (
+            <Link
+              key={ index }
+              to={ `/album/${music.collectionId}` }
+              data-testid={ `link-to-album-${music.collectionId}` }
+            >
+              <div className="Cards">
+                <p>
+                  { music.artistName}
+                </p>
+                <p>
+                  { music.collectionName}
+                </p>
+                <img
+                  src={ music.artworkUrl100 }
+                  alt={ music.artistName }
+                />
+              </div>
+            </Link>
+          ))
+        )
+        : <span>Nenhum álbum foi encontrado</span> }
+
     </div>
   );
 }
